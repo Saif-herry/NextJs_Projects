f9a411f03b43c36d85ec112c203a0f0a0c4da1c1
Mohammad Saif <saifsami321md@gmail.com>
2023-03-26 23:49:18 +0530

    navbar

---
+++ first_project_on_nextjs/pages/blog/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+      <h1>This is blog page</h1>
+    </div>
+  )
+}
+
+export default Product
--- first_project_on_nextjs/pages/blog/index.js
+++ first_project_on_nextjs/pages/blog/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const index = () => {
   return (
     <div>
-      <h1>This is Index page</h1>
+      
     </div>
   )
 }
