6df613251d8027cd8d6801dcb5f8bdd7158e52a6
Mohammad Saif <saifsami321md@gmail.com>
2023-04-14 00:17:06 +0530

    product page work progress

--- amazon-app/pages/index.tsx
+++ amazon-app/pages/index.tsx
@@ -1,9 +1,15 @@
-
-import Layout from "../components/Layout"
-export default function Home(){
-  return(
+import Layout from '../components/Layout';
+import data from '../Utils/data';
+import ProductItem from '../components/ProductItem';
+export default function Home() {
+  return (
     <Layout title="Home Page">
-     Home Page
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+
+          <ProductItem product = {product} key={product.title}></ProductItem>
+        ))}
+      </div>
     </Layout>
-  )
+  );
 }
