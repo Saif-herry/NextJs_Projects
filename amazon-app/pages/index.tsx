c31ef8746d2cd36ed291f31d8d0ad4f1d7e1e161
Mohammad Saif <saifsami321md@gmail.com>
2023-04-23 23:45:16 +0530

    update

--- amazon-app/pages/index.tsx
+++ amazon-app/pages/index.tsx
@@ -1,10 +1,10 @@
-import Layout from '../components/Layout';
-import data from '../Utils/data';
-import ProductItem from '../components/ProductItem';
+import Layout from '../components/Layout'
+import data from '../Utils/data'
+import ProductItem from '../components/ProductItem'
 export default function Home() {
   return (
     <Layout title="Home Page">
-      <div className="grid grid-cols-1 gap-10  md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-8  md:grid-cols-3 lg:grid-cols-4">
         {data.products.map((product) => (
           <ProductItem product={product} key={product.title}></ProductItem>
         ))}
