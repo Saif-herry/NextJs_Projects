9ee3323c40d88f69e571179992b9c1d9dc453765
Mohammad Saif <saifsami321md@gmail.com>
2023-05-01 20:02:38 +0530

    update

--- amazon-app/pages/product/[id].js
+++ amazon-app/pages/product/[id].js
@@ -2,6 +2,8 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import { useRouter } from 'next/router'
 import data from '../../Utils/data'
+import Link from 'next/link'
+import Image from 'next/legacy/image'
 
 const ProductScreen = () => {
   const { query } = useRouter()
@@ -15,7 +17,20 @@ const ProductScreen = () => {
   }
   return (
     <Layout title={product.name}>
-      <h1>{product.title}</h1>
+      <div className='py-2'>
+      <Link href="/">Back to Product</Link>
+      </div>
+
+      <div className='md:col-span-2'>
+        <Image src={product.image}
+        alt={product.title}
+        width={640}
+        height={640}
+        layout="responsive">
+
+
+        </Image>
+      </div>
     </Layout>
   )
 }
